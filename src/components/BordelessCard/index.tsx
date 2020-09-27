a169d4d5781a28f7923c0d111fd42c8e31efd6d5
bkkater <biancamedeiros.bs10@gmail.com>
2020-09-26 23:04:45 -0300

    [Add] Bordeless Card

---
+++ src/components/BordelessCard/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import verticalLine from '../../assets/icons/vertical-line.svg'
+
+import './styles.css';
+
+function BordelessCard() {
+    return (
+        <div>
+            <img src={verticalLine} alt="" />
+
+            <div>
+                <h2>O seu processo está no <strong>TRF5</strong></h2>
+
+                <p>Outra explicação blá blá blá Outra explicação blá blá blá Outra explicação blá blá blá  Outra explicação blá blá blá Outra explicação blá blá blá Outra explicação blá blá blá Outra explicação blá blá blá Outra explicação blá blá blá Outra explicação blá blá blá</p>
+            </div>
+
+        </div>
+    );
+}
+
+export default BordelessCard;
--- src/components/SituationCard/index.tsx
+++ src/components/SituationCard/index.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 import './styles.css'
 
 import icon from '../../assets/icons/situation-icon.svg'
+import progressBar from '../../assets/icons/progress-bar.svg'
+import progressStartIcon from '../../assets/icons/progress-end-icon.svg'
+import progressEndIcon from '../../assets/icons/progress-start-icon.svg'
+import arrowDown from '../../assets/icons/arrow-down.svg'
 
 function SituationCard() {
     return (
         <div className='situationCard-container'>
             <h2 className='titleText-inWhite'>Situação atual do seu processo</h2>
-            <img src={icon} alt="" className='situation-icon'/>
+            <img src={icon} alt="" className='situation-icon' />
 
             <h4 className='processDescription'>O seu processo está assim</h4>
             <p className='data'>08/06/2020</p>
@@ -19,6 +23,19 @@ function SituationCard() {
             <p>Nada a fazer.</p>
 
             <h4 className='processDescription'>O progresso do processo</h4>
+            <h5>TODAS AS ETAPAS CONCLUIDAS</h5>
+            <img src={progressBar} alt="Barra de progresso" className='progress-bar' />
+
+            <div className='progress-icons'>
+                <img src={progressStartIcon} alt="" />
+                <img src={progressEndIcon} alt="" />
+            </div>
+
+            <div className="data-details">
+                <p>Mostrar movimentação e dados</p>
+                <img src={arrowDown} alt=""/>
+            </div>
+
 
 
         </div>
