f0bbbcf48ea93d039ffab255674e15b3579602e5
bkkater <biancamedeiros.bs10@gmail.com>
2020-09-26 23:17:13 -0300

    [Add] Information Card

---
+++ src/components/InformationCard/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './styles.css'
+
+import icon from '../../assets/icons/situation-icon.svg'
+import progressBar from '../../assets/icons/progress-bar.svg'
+import progressStartIcon from '../../assets/icons/progress-end-icon.svg'
+import progressEndIcon from '../../assets/icons/progress-start-icon.svg'
+import arrowDown from '../../assets/icons/arrow-down.svg'
+
+function InformationCard() {
+    return (
+        <div className='informationCard-container'>
+            <h2 className='titleText-inWhite'>Informações</h2>
+        </div>
+    );
+}
+
+export default InformationCard;
